36e1024f57b4a3f019525664d857449b9c0baa98
NumanBashir <sportybashir@gmail.com>
2022-10-13 10:29:09 +0200

    Initial Commit

--- src/Checkers/Checkers.java
+++ src/Checkers/Checkers.java
@@ -33,16 +33,16 @@ public class Checkers extends JFrame { //Checkers class begins, extends on JFram
         game.add(board);
         game.add(board.title);
         game.add(board.newGame);
-        game.add(board.howToPlay);
-        game.add(board.credits);
+        //game.add(board.howToPlay);
+        //game.add(board.credits);
         game.add(board.message);
 
         //places components on the frame in the correct places
         board.setBounds(0,80,324,324);
         board.title.setBounds(0,0,324,50);
-        board.newGame.setBounds(6, 50, 100, 30);
-        board.howToPlay.setBounds(112, 50, 100, 30);
-        board.credits.setBounds(218, 50, 100, 30);
+        board.newGame.setBounds(115, 50, 100, 30);
+        //board.howToPlay.setBounds(112, 50, 100, 30);
+        //board.credits.setBounds(218, 50, 100, 30);
         board.message.setBounds(0, 404, 324, 30);
 
     }
